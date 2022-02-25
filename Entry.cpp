5d2552b9acf46567c971a24ad4bd3d466f28e548
Jaden Stanton <68559776+Jadenstanton@users.noreply.github.com>
2022-02-25 14:11:57 -0600

    Class for Entries
    Definitions are not implemented.

---
+++ Entry.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Jaden Stanton on 2/25/22.
+//
+
+#include "Entry.h"
+#include "linkedList.h"
+
+void Entry::addEntry() {
+//    addEntry(head);
+};
+
+void Entry::editEntry() {};
+
+void Entry::deleteEntry() {};
+
+void Entry::searchEntry() {};
+
+void Entry::viewAllEntries() {};
---
+++ Entry.h
@@ -0,0 +1,26 @@
+//
+// Created by Jaden Stanton on 2/25/22.
+//
+
+#ifndef DIVERSITYEQUITYINLCUSION_ENTRY_H
+#define DIVERSITYEQUITYINLCUSION_ENTRY_H
+#include "linkedList.h"
+
+class Entry: public Node {
+public:
+    void addEntry();
+    //Function for adding entry
+    void deleteEntry();
+    //Function for deleting entry
+    void editEntry();
+    //function for editing entry
+    void searchEntry();
+    //function for searching entry
+    void viewAllEntries();
+    //function for viewing all entries in category
+
+
+};
+
+
+#endif //DIVERSITYEQUITYINLCUSION_ENTRY_H
