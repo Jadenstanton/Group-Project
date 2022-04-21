a590ee6732181ba639940d4fb96027bdb1250cf2
Jaden Stanton <68559776+Jadenstanton@users.noreply.github.com>
2022-04-21 09:25:19 -0500

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,24 @@
+//
+// Created by Jaden Stanton on 4/19/22.
+//
+
+#ifndef DIVERSITYEQUITYINLCUSION_QUEUE_H
+#define DIVERSITYEQUITYINLCUSION_QUEUE_H
+#include <queue>
+
+class Queue {
+public:
+    void queuePush();
+    //function to push elements to queue
+    void isEmpty();
+    //function to check if queue is full or empty
+    void selection();
+    //function for selection
+    void queuePrint();
+    //function to print queue
+private:
+    std::queue<std::string> q;
+};
+
+
+#endif //DIVERSITYEQUITYINLCUSION_QUEUE_H
