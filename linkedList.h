0fcb1879d22a75d7bdd1a48b451c11e720414d64
Jaden Stanton <68559776+Jadenstanton@users.noreply.github.com>
2022-04-26 10:02:04 -0500

    Update linkedList.h
    Updated to include merge sort.

--- linkedList.h
+++ linkedList.h
@@ -1,53 +1,61 @@
 //
 // Created by Jaden Stanton on 2/22/22.
 //
-
+//Parent class for category class - Jaden Stanton
 #ifndef DIVERSITYEQUITYINLCUSION_LINKEDLIST_H
 #define DIVERSITYEQUITYINLCUSION_LINKEDLIST_H
 
 #include <string>
+#include <utility>
 
-//template<class nodeType>
 class Node {
 
 public:
+    Node*  accessHead() const { return(head);}
+    //accessor function for head
     void addItem(Node *n);
-    //function for adding entries
+    //function for adding entries - Jaden Stanton
     int listLength(Node *n);
-    //function for retrieving amount of entries
+    //function for retrieving amount of entries - Jaden Stanton
     void printList(Node *n);
-    //function for displaying info
+    //function for displaying info - Jaden Stanton
     void isEmpty(Node *n);
-    //function for checking whether section is empty or not
+    //function for checking whether section is empty or not - Jaden Stanton
     void search(Node *n, std::string item);
-    //function for searching for specific category
+    //function for searching for specific category - Jaden Stanton
     //this might be irrelevant
     void retrieveFirst(Node *n);
-    //function for retrieving the latest entry
+    //function for retrieving the latest entry - Jaden Stanton
     void retrieveLast(Node *n);
-    //function for retrieving the earliest entry
+    //function for retrieving the earliest entry - Jaden Stanton
     void retrieve(Node *n);
-    //function for retrieving specific entry
+    //function for retrieving specific entry - Jaden Stanton
     //may be irrelevant
     void deleteItem(Node *n, std::string item);
-    //function for deleting entry
+    //function for deleting entry - Jaden Stanton
     //needs to be verified
     void destroyList(Node *n);
-    //function for deleting all data in category
+    //function for deleting all data in category - Jaden Stanton
+    void mergeSort(Node *n);
+    //function for sorting stats - Jaden Stanton
+    Node* sortedMerge(Node *a, Node *b);
+    //function for mergeSort
+    void halfSplit(Node* source, Node** frontRef, Node** backRef);
+    //function for mergeSort
+
 
     // Default constructor
-    Node(){
+Node(){
         info = "EMPTY";
         link = nullptr;
     }
 
     // Parameter Constructor
-explicit Node(int data){
+explicit Node(std::string data){
         this->info = std::move(data);
         this->link = nullptr;
     }
-
-private:
+protected:
     std::string info;
     Node* link{};
     Node *tmp = nullptr;
